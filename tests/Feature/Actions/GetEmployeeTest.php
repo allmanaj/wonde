ffdd25307565336e96049f0b104b7c2d7315e8e0
Angus Allman <angus.allman@perchpeek.com>
2023-04-12 10:22:50 +0100

    Add fixtures to employee test

--- tests/Feature/Actions/GetEmployeeTest.php
+++ tests/Feature/Actions/GetEmployeeTest.php
@@ -4,8 +4,14 @@ use App\Actions\GetEmployee;
 use Illuminate\Support\Str;
 use Saloon\Exceptions\Request\Statuses\NotFoundException;
 use Saloon\Exceptions\SaloonException;
+use Saloon\Http\Faking\MockResponse;
+use Saloon\Laravel\Facades\Saloon;
 
 it('loads an employee\'s details from the wonde api', function(string $apiId, string $firstName, string $lastName) {
+
+    Saloon::fake([
+       MockResponse::fixture(sprintf('employee.%s', $apiId)),
+    ]);
 
     expect(resolve(GetEmployee::class)->execute($apiId))
         ->id->toEqual($apiId)
@@ -18,5 +24,8 @@ it('loads an employee\'s details from the wonde api', function(string $apiId, st
 ]);
 
 it('throws an exception if the request failed for whatever reason', function(){
+    Saloon::fake([
+       MockResponse::fixture('employee.invalid'),
+    ]);
     resolve(GetEmployee::class)->execute(Str::random(5));
 })->throws(SaloonException::class);
